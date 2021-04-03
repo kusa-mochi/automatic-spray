e8b3ac920e86c2b53dfa53b2eee32013fbd3e950
kusa-mochi <whoatemyapplepie@gmail.com>
2021-04-03 11:41:13 +0900

    決め打ちの値をマクロ化。コード整形。

--- automatic-spray/automatic-spray.ino
+++ automatic-spray/automatic-spray.ino
@@ -1,10 +1,12 @@
 #include <Servo.h>
+#define loopInterval 100  // loop関数のループ間隔[msec]
 #define echoPin 2
 #define trigPin 4
 #define servoPin 3
 #define sonicSpeed 340 // 音速[m/sec]
-#define servoOnDegree 1
-#define servoOffDegree 0
+#define switchDistance 10.0 // サーボモータの角度のON/OFFを切り替える基準とする距離[cm]
+#define servoOnDegree 1 // サーボモータONのときの角度[deg]
+#define servoOffDegree 0  // サーボモータOFFのときの角度[deg]
 
 double duration = 0; //受信した間隔
 double distance = 0; //距離
@@ -20,22 +22,22 @@ void setup() {
 void loop() {
   digitalWrite(trigPin, LOW);
   delayMicroseconds(2);
-  digitalWrite( trigPin, HIGH ); //超音波を出力
-  delayMicroseconds( 10 ); //
-  digitalWrite( trigPin, LOW );
-  duration = pulseIn( echoPin, HIGH ); //センサからの入力
+  digitalWrite(trigPin, HIGH); //超音波を出力
+  delayMicroseconds(10); //
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin, HIGH); //センサからの入力
   if (duration > 0) {
     duration = duration / 2; //センサから対象物までの片道経過時間
     distance = duration * sonicSpeed * 100 / 1000000; // 経過時間を距離に換算する。
     Serial.print(distance);
     Serial.println(" cm");
 
-    // 距離が10cm未満である場合
-    if(distance < 10.0) {
+    // 距離が<switchDistance>cm未満である場合
+    if (distance < switchDistance) {
       gripServo.write(servoOnDegree);
     } else {
       gripServo.write(servoOffDegree);
     }
   }
-  delay(100);
+  delay(loopInterval);
 }
